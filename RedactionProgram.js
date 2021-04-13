3b0d92f88086ff8055600ce81c6d803434053e6c
kavenarango <kavenarango@hotmail.com>
2021-04-13 18:06:26 -0400

    used jsdoc to comment code

--- RedactionProgram.js
+++ RedactionProgram.js
@@ -1,71 +1,90 @@
-// let myRegex = /[a-zA-z ]+/g;
-// ([a-zA-z]+)|([a-zA-z ]+[^"', ]*)
-// [^\s"',]+|"([^"]*)"|'([^']*)';
-// ([\w]+)|([\w]+[^'",])
-// ((?<=')[^,']+(?='))|((?<=")[^,"]+(?="))|([\w]+)
-// ((?<='|")[^,'"]+(?='|"))|([\w]+)
-
-
 class RedactDocuments {
-    constructor(Non_redacted_documents_path = '.\\Non-redacted_documents\\', Redacted_documents_path = '.\\Redacted_documents\\', Key_words = "Key_words.txt", replacementString = "XXXX") {
+    constructor(
+        document_Input_Path = '.\\Non-redacted_documents\\',
+        document_Output_Path = '.\\Redacted_documents\\',
+        key_Words_To_Redact = "Key_words.txt",
+        replacementString = "XXXX"
+    ) {
         this.RedactedWordRegex = /((?<='|")[^,'"]+(?='|"))|([\w]+)/gmi;
-        this.Non_redacted_documents_path = Non_redacted_documents_path
-        this.Redacted_documents_path = Redacted_documents_path
-        this.Key_words = Key_words
+        this.document_Input_Path = document_Input_Path
+        this.document_Output_Path = document_Output_Path
+        this.key_words_to_redact = key_Words_To_Redact
         this.replacementString = replacementString
-        this.redactionRegex = new RegExp(this.BuildRegexForRedaction(), "gmi")
     }
 
+    /**
+     * @param {String} file The name of the file you want to read from 
+     * @param {string} path A path if file does not already provide a path
+     * @description Will open and read the file given to it
+    */
     OpenFile(file, path = "") {
         const fs = require('fs');
-        return (fs.readFileSync((path + file), 'utf-8', function (err) { if (err) throw err; }))
+        return (fs.readFileSync((path + file), 'utf-8', function (err) { if (err) throw err; })) // reads the file contents
     }
 
+    /**
+     * @param {string} param Path of the folder you want to collect from 
+     * @returns (path + file name)
+     * @default param uses document input path as default
+     * @description opens a folder and collects the names of the files in the folder 
+     */
+    OpenFolderForCollection(path = this.document_Input_Path) {
+        const fs = require('fs');
+        return [path + fs.readdirSync(path, function (err) { if (err) throw err; })]
+    }
+
+    /**
+     * @param {Array} documents Array of strings holding path to a file
+     * @description This function takes an array of files with their path redacting the words and outputting the file to the ouput path
+     */
     RedactDocuments(documents) {
         var fs = require('fs');
-        let foundFailedDocuments = false
-        let FailedDocuments = []
-        const FileNameRegex = /[\w.]+$/;
+        let redactionRegex = this.BuildRegexForRedaction() //Creates the regex used for redaction based on the file with the keywords
+        const FileNameRegex = /[\w.]+$/; // Regex to get the name of the file from the path with the files extention
+        let failedDocuments = ["Failed Documents"] // array of failed to redact documents
+
         for (var i in documents) {
             try {
-                let document = this.OpenFile(documents[i])
-                let RedactedDocument = document.replace(this.redactionRegex, this.replacementString);
-                fs.writeFile((this.Redacted_documents_path + documents[i].match(FileNameRegex)), RedactedDocument, function (err) { if (err) throw err; });
-            } catch
-            {
-                FailedDocuments.push(documents[i].match(FileNameRegex))
-                foundFailedDocuments = true
+                let documentPathAndName = documents[i]
+                let documentName = documentPathAndName.match(FileNameRegex) // will parse out the file name from the full path
+                let document = this.OpenFile(documentPathAndName)
+                let RedactedDocument = document.replace(redactionRegex, this.replacementString); // finds all the matches and replaces them with the Replacment string
+                fs.writeFile((this.document_Output_Path + documentName), RedactedDocument, function (err) { if (err) throw err; }); // will write the redacted document to the output path will not check if file exists 
+            }
+            catch {
+                failedDocuments.push(documentPathAndName) // gives a log of any failed to redact documents
+                continue;
             }
         }
-        if (foundFailedDocuments) {
-            throw FailedDocuments
+        if (failedDocuments.length > 1) { // checks if there are any failed documents and throws them
+            throw failedDocuments
         }
     }
 
-    GetDocumentsForRedaction() {
-        const fs = require('fs');
-        return [this.Non_redacted_documents_path + fs.readdirSync(this.Non_redacted_documents_path, function (err) { if (err) throw err; })]
-
-    }
-
+    /**
+     * @description This will use the textfile given with the words to redact from documents and will build a regex that will be used to find the words in the documents
+     * @returns regex that will be used to redact words from documents
+     */
     BuildRegexForRedaction() {
-        const match = this.OpenFile(this.Key_words).match(this.RedactedWordRegex)
+        const match = this.OpenFile(this.key_words_to_redact).match(this.RedactedWordRegex)
         var RedactedWordsForDoc = ""
         for (var i in match) {
             RedactedWordsForDoc += (match[i] + "|")
         }
         RedactedWordsForDoc = RedactedWordsForDoc.slice(0, -1)
-        return RedactedWordsForDoc
+        return new RegExp(RedactedWordsForDoc, "gmi")
     }
 
+    /**
+     * @description This will handle all of the redactions once the object has been made with the approprite paramters
+     */
     DoAllDocumentRedaction() {
-        this.RedactDocuments(this.GetDocumentsForRedaction())
+        this.RedactDocuments(this.OpenFolderForCollection())
     }
 }
 
-
 try {
-    let Redactor = new RedactDocuments(undefined, undefined, "Other_Key_words.txt")
+    let Redactor = new RedactDocuments(undefined, undefined, "Key_words.txt")
     Redactor.DoAllDocumentRedaction()
 }
 catch (err) {
